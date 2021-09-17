4c6f9dc416cf4b55874543d0b441d364bda4dac2
Turan Soyuer <soyuer.turan@gmail.com>
2021-09-17 14:38:25 +0300

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnalyticService } from './service/analytic.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'perfanalytics-app';
+
+  constructor(private analyticService: AnalyticService) {}
 }
