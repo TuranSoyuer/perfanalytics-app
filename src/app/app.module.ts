ddd9b14c0a8277aa4eca472c26530fedb2bab5db
TuranS <turans@garantibbva.com.tr>
2021-09-18 00:11:51 +0300

    Bootstrap and dashboard components added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LineChartComponent
   ],
   imports: [
     BrowserModule,
