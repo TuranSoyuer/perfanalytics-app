78b6f7043bce6262d33547a32ccb402dad0cdb9c
TuranS <turans@garantibbva.com.tr>
2021-09-17 22:50:16 +0300

    Service added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { AnalyticService } from './service/analytic.service';
+import {Component, OnInit} from '@angular/core';
+import {AnalyticItem, AnalyticService} from './service/analytic.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'perfanalytics-app';
+  analyticItems: AnalyticItem[] = [];
 
-  constructor(private analyticService: AnalyticService) {}
+
+  constructor(private analyticService: AnalyticService) {
+  }
+
+  ngOnInit(): void {
+    this.loadAnalytics();
+  }
+
+  loadAnalytics(): Subscription {
+    console.log("load analytics");
+    return this.analyticService.getAnalytics().subscribe(data => {
+      console.log(JSON.stringify(data));
+      this.analyticItems = data;
+    })
+  }
+
 }
--- src/app/service/analytic.service.ts
+++ src/app/service/analytic.service.ts
@@ -1,13 +1,53 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError, retry} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyticService {
+  private apiUrl: string = "https://performanceanalytics-api.herokuapp.com/api";
 
-  constructor() { }
-
-  getAnalytics() {
-    
+  constructor(private http: HttpClient) {
   }
+
+  getAnalytics(): Observable<AnalyticItem[]> {
+    return this.http.get<AnalyticItem[]>(this.apiUrl + "/analytics")
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+
+  // Error handling
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+}
+
+export interface AnalyticItem {
+  siteUrl: string,
+  ttfb: number,
+  fcp: number,
+  domLoad: number,
+  windowLoad: number,
+  resourceMetrics: ResourceMetric[],
+  createDate: string
+}
+
+export interface ResourceMetric {
+  name: string,
+  duration: number,
+  initiatorType: string,
+  transferSize: number
 }
